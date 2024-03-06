5312bd4d51a7b751842ae11cb75a03466bf70df2
reny <reny@yxsd.com.cn>
2024-03-06 16:09:15 +0800

    adjust

--- algorithms/neural_networks/LSTM.py
+++ algorithms/neural_networks/LSTM.py
@@ -1,20 +1,95 @@
+import os
+import sys
+
+import matplotlib.pyplot as plt
+import torch
 import torch.nn as nn
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from torch.optim import Adam
+from tqdm import tqdm
 
 
 class LSTM(nn.Module):
-
-    def __init__(self, input_size=(4, 2), hidden_size=2, num_layers=2):
-        print("input_size", input_size)
+    def __init__(self, input_size, hidden_size, output_size, num_layers=2, batch_first=True):
+        """
+        :param input_size: 输入样本特征数量
+        :param hidden_size: 每步的隐藏节点个数
+        :param output_size: 输出的大小
+        :param num_layers: 步长（LSTM个数）
+        :param batch_first:
+                如果为True:输入数据为(batch_size, seq_length, input_size),否则为(seq_length, batch_size, input_size)
+        """
         super(LSTM, self).__init__()
-        self.lstm1 = nn.LSTM(input_size=input_size[0], hidden_size=hidden_size, num_layers=num_layers)
-        self.lstm2 = nn.LSTM(input_size=input_size[1], hidden_size=hidden_size, num_layers=num_layers)
-        self.lr = nn.Linear(hidden_size, hidden_size)
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers,
+                            batch_first=batch_first)
+        self.lr = nn.Linear(hidden_size, output_size)
 
-    def forward(self, x):
-        print("x", x.shape)
-        output, (hn, cn) = self.lstm1(x)
-        output, (hn, cn) = self.lstm2(output)
-        output = self.lr(output)[:, -1]
+    def forward(self, X):
+        output, _ = self.lstm(X)
+        output = self.lr(output[:, -1, :])
         return output
 
 
+def LSTM_train(input_size, hidden_size, output_size, num_layers, data_train, data_test, labels_train, labels_test,
+               save_path, batch_first=True):
+
+    """
+
+    :param input_size: 输入大小-特征个数
+    :param hidden_size: 每步的隐藏节点个数
+    :param output_size: 输出大小
+    :param num_layers: 步数（LSTM模块个数）
+    :param data_train:训练数据
+    :param data_test:测试数据
+    :param labels_train:训练标签
+    :param labels_test:测试标签
+    :param save_path:模型保存路径
+    :param batch_first:是否batch优先，
+                如果为True:输入数据为(batch_size, seq_length, input_size),否则为(seq_length, batch_size, input_size)
+    :return:
+    """
+    # 对训练集训练，保存模型，并返回测试集测试结果
+    x = torch.tensor(data_train, dtype=torch.float)
+    y = torch.tensor(labels_train, dtype=torch.float)
+
+    x_t = torch.tensor(data_test, dtype=torch.float)
+    y_t = torch.tensor(labels_test, dtype=torch.float)
+
+    model = LSTM(input_size, hidden_size, output_size, num_layers=num_layers, batch_first=batch_first)
+    loss_func = torch.nn.MSELoss()
+    optimizer = Adam(params=model.parameters(), lr=0.01)
+    itr = 50
+    iter_loss = []
+    for i in tqdm(range(itr)):
+        pred = model(x)
+        loss = loss_func(pred, y)
+        iter_loss.append(float(loss))
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    plt.plot(range(len(iter_loss)), iter_loss, label='loss')
+    plt.show()
+
+    # 保存
+    torch.save(model, save_path)
+
+    pred = model(x_t)
+    acc = loss_func(pred, y_t)
+    acc = float(acc)
+    print("训练测试acc", acc)
+    return acc
+
+
+def LSTM_test(datatest, read_path):
+    """
+    对测试集测试
+    :param datatest:
+    :param read_path:
+    :return:
+    """
+    x_t = torch.tensor(datatest, dtype=torch.float)
+    model = torch.load(read_path)
+    pred = model(x_t)
+    return pred.detach().numpy()
--- tests/LSTM_test.py
+++ tests/LSTM_test.py
@@ -1,19 +1,52 @@
 import os
 import sys
-import torch
-dir_path = os.path.dirname(os.path.abspath(sys.path[0]))
-sys.path.append(dir_path)
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+sys.path.append(os.path.dirname(os.path.abspath(sys.path[0])))
 from algorithms.neural_networks.LSTM import *
 
+if __name__ == '__main__':
+    """
+    目标：
+    1000条数据，特征数为5，用LSTM，4步预测3步，包含数据集的切分、模型训练、测试，torch实现。
+    # model = LSTM(input_size=5, hidden_size=2, output_size=3, num_layers=4, batch_first=True)
+    # output = model(X)
+    # print(X.shape)
+    # print(output.shape)
+    """
+    """
+    参数定义
+    """
+    input_size, hidden_size, output_size, num_layers = 5, 2, 3, 4
 
-if __name__=='__main__':
-    # 实例化 LSTM 模型
-    lstm_model = LSTM(input_size=(4, 2), hidden_size=2, num_layers=2)
-    print(dir(lstm_model))
-    # 创建输入张量
-    input_tensor = torch.randn(2, 2, 4)  # 输入维度为 (sequence_length, batch_size, input_size)
-    # 前向传播获取输出
-    output = lstm_model(input_tensor)
+    """
+    构造数据集，示例
+    """
+    data = np.random.randn(1000, 5)
+    scaler = MinMaxScaler(feature_range=(-1, 1))
+    data = scaler.fit_transform(data)
 
-    # 打印输出张量的形状
-    print("输出张量形状:", output.shape)
+    use_step = 4
+    pre_step = 3
+    final_x_data = []  # (994, 4, 5)
+    final_y_data = []  # (994, 3)
+    for i in range(0, len(data) - pre_step - use_step + 1, 1):
+        final_x_data.append(data[i:i + use_step])
+        final_y_data.append(data[i + use_step:i + use_step + pre_step])
+    final_x_data = np.array(final_x_data)
+    final_y_data = np.array(final_y_data)[:, :, -1]
+    X_train, X_test, y_train, y_test = train_test_split(final_x_data, final_y_data, test_size=0.3, random_state=42)
+
+    """
+    训练模型
+    """
+    save_path = os.path.join(sys.path[0], 'save_model', 'lstm.pt')
+    # LSTM_train(input_size, hidden_size, output_size, num_layers, X_train, X_test, y_train, y_test, save_path,
+    #            batch_first=True)
+
+    """
+    测试模型
+    """
+    pre_t = LSTM_test(X_test, save_path)
+    print(f"pre_t{pre_t.shape}")
