a028cd50e622928b581390cb078730c0995e396d
reny <reny@yxsd.com.cn>
2024-07-16 20:56:06 +0800

    add read txt plot 3d

--- utils/file_op.py
+++ utils/file_op.py
@@ -8,6 +8,19 @@ import pandas as pd
 用于文件读写、目录管理和文件格式转换的函数。
 用于管理数据集文件、保存和加载模型参数等。
 """
+
+def detect_encoding(path):
+    """
+    check encoding
+    :return:
+    """
+    with open(path, 'rb') as f:
+        raw_data = f.read()
+        result = chardet.detect(raw_data)
+        encoding = result['encoding']
+    return encoding
+
+
 class ExcelFileRead:
     """
     获取excel格式的数据
@@ -28,7 +41,7 @@ class ExcelFileRead:
 
 class CsvFileRead:
     """
-    获取excel格式的测试曲线数据
+    获取csv格式的测试曲线数据
     """
     @staticmethod
     def read_file(file_path):
@@ -44,6 +57,73 @@ class CsvFileRead:
         f_charinfo = chardet.detect(f.read())['encoding']
         curves_datas = pd.read_csv(open(file_path, encoding=f_charinfo))
         return curves_datas
+
+
+
+
+
+
+
+
+class TxtFileRead:
+    """
+    获取txt格式的测试曲线数据
+    """
+    @staticmethod
+    def detect_separator(file_path, num_lines=5, possible_separators=None):
+        """
+        自动检测txt中的分隔符
+        :param file_path:
+        :param num_lines:
+        :param possible_separators:常见分隔符
+        :return:
+        """
+        this_file_encoding = detect_encoding(file_path)
+        if this_file_encoding not in ['utf8', 'utf-8']:
+            this_file_encoding = 'gbk'
+        if possible_separators is None:
+            possible_separators = [',', '\t', ';', '|']
+        with open(file_path, 'r', encoding=this_file_encoding) as file:
+            lines = [file.readline() for _ in range(num_lines)]
+
+        best_guess = ' '
+        max_count = 0
+        for sep in possible_separators:
+            counts = [line.count(sep) for line in lines[0:1]]
+            if counts[0] > max_count:
+                best_guess = sep
+                max_count = counts[0]
+        return best_guess, this_file_encoding
+
+    @staticmethod
+    def clean_dataframe(df):
+        """
+        清理首尾空格
+        :param df:
+        :return:
+        """
+        df.columns = df.columns.str.strip()
+        df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+        return df
+
+    @staticmethod
+    def read_file(file_path):
+        """
+        解析多种分隔符的txt文档
+        :param file_path:字符串，包含文件路径和文件名的完整路径。
+        "./datasets/test.txt"
+        :return:
+        读取txt格式的数据，如果数据不存在，则返回pd.DataFrame()
+        """
+        if not os.path.exists(file_path):
+            return pd.DataFrame()
+        separator, this_file_encoding = TxtFileRead.detect_separator(file_path)
+        if separator == ' ':
+            df = pd.read_csv(file_path, delim_whitespace=True, index_col=False, encoding=this_file_encoding)
+        else:
+            df = pd.read_csv(file_path, delimiter=separator, index_col=False, encoding=this_file_encoding)
+        df = TxtFileRead.clean_dataframe(df)
+        return df
 
 
 
@@ -79,3 +159,8 @@ def glob_select_file(root_path, select_file_name):
     """
     files = glob.glob(os.path.join(root_path, '**', select_file_name), recursive=True)
     return files
+
+if __name__ == '__main__':
+    data_path = r"\\nas.yxsd\大数据产品部\辽河_小层识别_r\240715齐40智能地层划分\pre_信息所_齐40_智能对比sj\解释表/well2_IntpAct1.txt"
+    data = TxtFileRead.read_file(data_path)
+    print(data)
--- utils/plot_show.py
+++ utils/plot_show.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 from scipy.spatial import Voronoi, voronoi_plot_2d
 
 # 创建示例数据
@@ -28,7 +29,57 @@ def show_voronoi(points):
     # 显示图形
     plt.show()
 
+def plot_3d_data(df, x_col, y_col, z_col, color_by_list, colormap='viridis'):
+    """
+    生成3D散点图，显示X、Y、Z数据，并按color_by_list着色。
+    :param df: 包含井数据的Pandas DataFrame。
+    :param x_col: x坐标列名
+    :param y_col: y坐标列名
+    :param z_col: z坐标列名
+    :param color_by: [one_col, two_col, three_col]，如果有一列，则按这一列，如果有两列，则按两列
+    :param colormap: Matplotlib颜色映射名称。
+    :return:
+    """
+    # 创建3D图形
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111, projection='3d')
+
+    # 根据color_by参数选择颜色编码方式
+    color_by = '_'.join(color_by_list)
+    if len(color_by_list) > 1:
+        df[color_by] = df[color_by_list].apply(lambda row: '_'.join(row.astype(str)), axis=1)
+    colors = df[color_by].values
+
+    # 为每个不同的颜色分配一个唯一的颜色
+    unique_colors = np.unique(colors)
+    color_map = plt.get_cmap(colormap)
+    color_array = [color_map(i / len(unique_colors)) for i in range(len(unique_colors))]
+    color_dict = dict(zip(unique_colors, color_array))
+
+    # 绘制散点图
+    for color in unique_colors:
+        subset = df[df[color_by].apply(lambda x: str(x) if isinstance(x, int) else x) == str(color)]
+        ax.scatter(subset[x_col], subset[y_col], subset[z_col], c=color_dict[color], label=str(color), s=60)
+
+    # 设置图表标题和坐标轴标签
+    ax.set_title('3D Visualization of Well Data')
+    ax.set_xlabel(x_col)
+    ax.set_ylabel(y_col)
+    ax.set_zlabel(z_col)
+
+    # 添加图例
+    handles, labels = [], []
+    for l, c in color_dict.items():
+        handles.append(plt.Line2D([0], [0], marker='o', color='w', markerfacecolor=c, markersize=10))
+        labels.append(l)
+    ax.legend(handles, labels, loc='upper right')
+
+    plt.show()
+
 
 # if __name__ == '__main__':
-#     points = np.random.rand(10, 2)
-#     show_voronoi(points)
+#     # points = np.random.rand(10, 2)
+#     # show_voronoi(points)
+#
+#     data = pd.DataFrame({'X':[1, 2, 3, 5], 'Y':[1, 5, 3, 6], 'Z':[2, 4, 1, 6], 'name':['a', 'a', 'b', 'b'], 'id':['s', 's', 's', 'c']})
+#     plot_3d_data(data, 'X', 'Y', 'Z', color_by_list=['name', 'id'])
