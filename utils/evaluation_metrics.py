222fff2d13ce0cb5c697284d7e948b4e59adb60c
reny <reny@yxsd.com.cn>
2024-07-29 10:33:08 +0800

    add ROC and heatmap

--- utils/evaluation_metrics.py
+++ utils/evaluation_metrics.py
@@ -3,6 +3,12 @@ from typing import Callable
 from scipy.integrate import simps
 
 
+from sklearn.metrics import roc_curve, auc
+from sklearn.preprocessing import label_binarize
+from scipy import interp
+from itertools import cycle
+import numpy as np
+from matplotlib import pyplot as plt
 """
 评价指标
 """
@@ -87,3 +93,116 @@ def compute_bending_energy(y):
 
 
 
+def plot_multiclass_roc(y_true, y_score, classes, title='Receiver operating characteristic', map_dict=None):
+    """
+    绘制多分类任务中的ROC曲线
+    :param y_true: 真实标签(n_samples,) or (n_samples, n_classes)
+    :param y_score: 预测概率标签(n_samples, n_classes)
+    :param classes: 预测的类别列表 (n_classes,)
+    :param title:标题
+    :param map_dict:类别映射
+    :return:
+    """
+    n_classes = len(classes)
+    if y_true.ndim == 1:
+        y_true = label_binarize(y_true, classes=range(n_classes))
+        if n_classes == 2:
+            y_true = np.hstack((1 - y_true, y_true))
+    # Compute ROC curve and ROC area for each class
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_true[:, i], y_score[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    # Compute micro-average ROC curve and ROC area
+    fpr["micro"], tpr["micro"], _ = roc_curve(y_true.ravel(), y_score.ravel())
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+    # Compute macro-average ROC curve and ROC area
+    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+    mean_tpr = np.zeros_like(all_fpr)
+    for i in range(n_classes):
+        mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+    mean_tpr /= n_classes
+
+    fpr["macro"] = all_fpr
+    tpr["macro"] = mean_tpr
+    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+    # Plot all ROC curves
+    plt.figure()
+    colors = cycle(['aqua', 'darkorange', 'cornflowerblue', 'green', 'red', 'purple', 'brown', 'pink', 'gray', 'olive'])
+    for i, color in zip(range(n_classes), colors):
+        class_name = classes[i]
+        if map_dict is not None and class_name in map_dict:
+            class_name = map_dict[class_name]
+        plt.plot(fpr[i], tpr[i], color=color, lw=2,
+                 label='ROC curve of class {0} (area = {1:0.2f})'
+                       ''.format(class_name, roc_auc[i]))
+
+    plt.plot(fpr["micro"], tpr["micro"], color='deeppink', linestyle=':', linewidth=4,
+             label='micro-average ROC curve (area = {0:0.2f})'
+                   ''.format(roc_auc["micro"]))
+
+    plt.plot(fpr["macro"], tpr["macro"], color='navy', linestyle=':', linewidth=4,
+             label='macro-average ROC curve (area = {0:0.2f})'
+                   ''.format(roc_auc["macro"]))
+
+    plt.plot([0, 1], [0, 1], 'k--', lw=2)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title(title)
+    plt.legend(loc="lower right")
+    plt.show()
+
+
+
+def plot_binary_roc(y_true, y_score, title='Receiver Operating Characteristic'):
+    """
+    绘制二分类的roc曲线
+    :param y_true:真实标签(n_samples,)
+    :param y_score:预测标签（1类）(n_samples,)
+    :param title:标题
+    :return:
+    """
+    fpr, tpr, _ = roc_curve(y_true, y_score)
+    roc_auc = auc(fpr, tpr)
+
+    # Plot ROC curve
+    plt.figure()
+    plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = {0:0.2f})'.format(roc_auc))
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title(title)
+    plt.legend(loc="lower right")
+    plt.show()
+
+#
+# if __name__ == '__main__':
+#     # 示例数据
+#     from sklearn.datasets import make_classification
+#     from sklearn.model_selection import train_test_split
+#     from sklearn.preprocessing import label_binarize
+#     from sklearn.svm import SVC
+#     from sklearn.multiclass import OneVsRestClassifier
+#
+#     classes = [0, 3]
+#     # 生成一个示例数据集
+#     X, y = make_classification(n_samples=1000, n_features=20, n_classes=len(classes), n_informative=3, n_clusters_per_class=1)
+#
+#     # 数据集分割
+#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=0)
+#
+#     # 使用One-vs-Rest分类器和SVM
+#     classifier = OneVsRestClassifier(SVC(kernel='linear', probability=True, random_state=0))
+#     y_score = classifier.fit(X_train, y_train).predict_proba(X_test)
+#     # 绘制ROC曲线
+#     if len(classes) == 2:
+#         plot_binary_roc(y_test, y_score[:, 1])
+#     plot_multiclass_roc(y_test, y_score, classes)
+#
--- utils/seaborn_show.py
+++ utils/seaborn_show.py
@@ -20,7 +20,46 @@ def plot_jointplot(data, x, y, kind='scatter', **kwargs):
     plt.show()
 
 
-# if __name__ == '__main__':
-#     # 示例用法
-#     tips = sns.load_dataset("tips")
-#     plot_jointplot(data=tips, x="total_bill", y="tip", kind="scatter")
+def plot_heatmap(data, row_labels, col_labels, title='Heatmap', cmap='viridis'):
+    """
+    Plot a heatmap using seaborn.
+
+    Parameters:
+    - data: 2D array-like
+        The data matrix to be visualized.
+    - row_labels: list of str
+        The labels for the rows.
+    - col_labels: list of str
+        The labels for the columns.
+    - title: str, optional (default='Heatmap')
+        The title of the heatmap.
+    - cmap: str, optional (default='viridis')
+        The colormap to be used for the heatmap.
+    """
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(data, xticklabels=col_labels, yticklabels=row_labels, cmap=cmap, annot=True, fmt=".2f")
+    plt.title(title)
+    plt.xlabel('Columns')
+    plt.ylabel('Rows')
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    # # 示例用法
+    # tips = sns.load_dataset("tips")
+    # plot_jointplot(data=tips, x="total_bill", y="tip", kind="scatter")
+
+    # 示例数据
+    data = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 16]
+    ]
+
+    row_labels = ['Row 1', 'Row 2', 'Row 3', 'Row 4']
+    col_labels = ['Col 1', 'Col 2', 'Col 3', 'Col 4']
+
+    # 绘制热力图
+    plot_heatmap(data, row_labels, col_labels, title='Example Heatmap', cmap='YlGnBu')
